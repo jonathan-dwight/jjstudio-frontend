9915141b78c321328bc63d7ea04bace62751a5c3
jonathan-dwight <jd.buendia0711@gmail.com>
2020-11-18 12:13:46 -0800

    Add Form

--- src/components/session/session_form.jsx
+++ src/components/session/session_form.jsx
@@ -11,12 +11,71 @@ class SessionForm extends React.Component {
             password: "",
             password2: "",
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleInput(field) {
+        return (e) => this.setState({ [field]: e.currentTarget.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let user = this.state;
+
+        this.props.processForm(user).then(
+            () => {
+                this.props.closeModal();
+                this.props.history.push('/beats') // need to figure out what route to go to
+            }
+        )
+        // persist if something happens
+        this.setState({
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            email: this.state.email,
+            password: "",
+            password2: ""
+        })
     }
 
     render() {
+
+        const firstName = (this.props.formType === 'signup') ? (
+            <label>
+                <input type="text" value={this.state.firstname}/>
+            </label>
+        ) : ( null )
+
+        const lastName = (this.props.formType === 'signup') ? (
+            <label>
+                <input type="text" value={this.state.lastname} />
+            </label>
+        ) : (null)
+
+        const secondPassword = (this.props.formType === 'signup') ? (
+            <label>
+                <input type="text" value="" />
+            </label>
+        ) : (null)
+        
+        // figure out where to autofocus
+
         return (
             <>
-                <div>Hello World</div>
+                <div className="login-form-container">
+                    <form className="login-form-box">
+                        <div>
+                            <h1>JJ Studio</h1>
+                            <p>Make beats</p>
+                        </div>
+                        {firstName}
+                        {lastName}
+                        <label> Email:
+                            <input  type="text" onChange={this.handleInput('email')}/>
+                        </label>
+                    </form>
+                </div>
             </>
 
         )
