a8836812a910c6dbd9601debe131c6d60f7db44d
jonathan-dwight <jd.buendia0711@gmail.com>
2020-11-17 21:39:46 -0800

    Add All Root Files

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <>
+            <div>Hello World</div>
+        </>
+    )
+}
+
+export default App;
+
+//PLACE HOLDER RIGHT NOW NEED TO WORK ON LOGIN AND SIGN UP AND THEN MODAL FOR IT
--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux';
 import session from "./session_reducer";
 import errors from "./errors_reducer";
 
-
 //syntantic sugar for:
 // errors: errors
 const RootReducer = combineReducers({
     errors,
     session
 })
+
+export default RootReducer;
