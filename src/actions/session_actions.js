19321176dc564a2c4269b7713b24f613183e9f29
jonathan-dwight <jd.buendia0711@gmail.com>
2020-11-17 20:57:59 -0800

    add Session action and api util

---
+++ src/actions/session_actions.js
@@ -0,0 +1,57 @@
+import * as SessionAPIUtil from "../util/session_api_util";
+import jwt_decode from "jwt-decode";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
+
+// action creators
+export const receiveCurrentUser = currentUser => ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+})
+
+export const receiveUserSignIn = () => ({
+    type: RECEIVE_USER_SIGN_IN
+});
+
+export const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+});
+
+export const logoutUser = () => ({
+    type: RECEIVE_USER_LOGOUT
+});
+
+// thunk action creators
+export const signup = user => dispatch => (
+    APIUtil.signup(user).then(() => {
+        dispatch(receiveUserSignIn())
+    }
+    ).catch(err => (
+        dispatch(receiveErrors(err.response.data))
+    ))
+);
+
+// this is to set session token in local storage
+// but might change with how springboot works
+export const login = user => dispatch => (
+    APIUtil.login(user).then(res => {
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        APIUtil.setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(receiveCurrentUser(decoded))
+    })
+        .catch(err => {
+            dispatch(receiveErrors(err.response.data));
+        })
+)
+
+export const logout = () => dispatch => {
+    localStorage.removeItem('jwtToken');
+    APIUtil.setAuthToken(false);
+    dispatch(logoutUser());
+};
---
+++ src/util/session_api_util.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+
+//Will set the authToken if someone is logged in
+export const setAuthToken = (token) => {
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = token;
+    } else {
+        delete axios.defaults.headers.common['Authorization'];
+    }
+}
+
+export const signup = (userData) => {
+    return axios.post('/') // need to grab this from api route to signup a user
+}
+
+export const login = (userData) => {
+    return axios.post('/') // need to grab this for a login
+}
+
