77d1f6d7308746ebb4cdccd4dd692d4cf8227a31
jonathan-dwight <jd.buendia0711@gmail.com>
2020-11-23 19:41:42 -0800

    add reactstrap and navbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Modal from "./modal/modal"
 import { Route, Switch, Redirect } from 'react-router-dom';
 import HomeContainer from "./home/home_container"
+import AppNavBar from "./navbar/navbar"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
         <> 
             <div>
                 <Modal />
-
+                <AppNavBar />
                 <Switch>
                     <Route exact path = "/" component={HomeContainer} />
                 </Switch>
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+import './navbar.css'
+
+class AppNavbar extends React.Component {
+    state = {
+        isOpen: false
+    };
+
+    toggle = () => {
+        this.setState((prevState) => ({
+            isOpen: !prevState.isOpen
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm">
+                    <NavbarBrand href="/">JC Studio</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/justinchung/jcstudio">GitHub</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/">Sign In</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Settings
+              </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                </DropdownItem>
+                                    <DropdownItem divider />
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavbar;
